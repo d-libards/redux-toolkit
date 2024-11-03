5cd81f4ce74fc92d8391710b07ba4b52bd24b46c
d-libards <dominiquelibardo09@gmail.com>
2024-11-04 03:42:27 +0800

    add api call

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,29 @@ import CartContainer from './component/CartContainer';
 import Navbar from './component/Navbar';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import { calculateTotals } from './features/cart/cartSlice';
+import { calculateTotals, getCartItems } from './features/cart/cartSlice';
 import Modal from './component/Modal';
 
 function App() {
-  const { cartItems } = useSelector((state) => state.cart);
+  const { cartItems, isLoading } = useSelector((state) => state.cart);
   const { isOpen } = useSelector((state) => state.modal);
   const dispatch = useDispatch();
 
   useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
+
+  useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems]);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
 
   return (
     <main>
